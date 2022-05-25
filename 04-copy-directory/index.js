8f29d07d6f84c835a36275b19d4000181c3a3933
Коля Белорус <80271317+adabakidabra@users.noreply.github.com>
2022-05-25 15:13:18 +0300

    fix: js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,21 +2,26 @@ const fs = require("fs");
 const path = require("path");
 
 function copyDir() {
-  fs.mkdir("./04-copy-directory/files-copy", { recursive: true }, (err) => {
+  fs.rm("./04-copy-directory/files-copy",{ recursive: true, force: true }, (err) => {
     if (err) throw err;
-  });
-}
 
-fs.readdir("04-copy-directory/files", (err, data) => {
-  data.forEach((el) => {
-    fs.copyFile(
-      `${__dirname}/files/${el}`,
-      `${__dirname}/files-copy/${el}`,
-      (err) => {
-        if (err) throw err;
-      }
-    );
+    fs.mkdir("./04-copy-directory/files-copy", { recursive: true }, (err) => {
+      if (err) throw err;
+      fs.readdir("04-copy-directory/files", (err, data) => {
+        data.forEach((el) => {
+          fs.copyFile(
+            `${__dirname}/files/${el}`,
+            `${__dirname}/files-copy/${el}`,
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        });
+      });
+    });
   });
-});
+};
+
+
 
 copyDir();
